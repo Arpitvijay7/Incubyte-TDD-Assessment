2a1d441d7956cefd9d4d638876ca92ebb2ad499e
Arpitvijay7 <arpitvijay2002@gmail.com>
2024-10-11 12:00:39 +0530

    Added test for New Lines as Delimiters

--- src/calculator.ts
+++ src/calculator.ts
@@ -1,6 +1,7 @@
 export function add(numbers: string): number {
     if (numbers === "") return 0;
-    const numArray = numbers.split(",").map(Number);
+    const delimiters = /[,\n]/;
+    const numArray = numbers.split(delimiters).map(Number);
     return numArray.reduce((a, b) => a + b, 0);
   }
   
--- tests/calculator.test.ts
+++ tests/calculator.test.ts
@@ -17,4 +17,7 @@ describe('String Calculator', () => {
     expect(add("1,2,3,4,5")).toBe(15);
   });
   
+  test('should handle new lines as delimiters', () => {
+    expect(add("1\n2,3")).toBe(6);
+  });
 });
